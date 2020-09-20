e94a667911736259e92f376b9f8bcfbdfa627ee2
Balkrishna Agarwal <agarwalbalkrishnaindia@gmail.com>
2020-09-20 22:58:57 +0530

    static calandar & popup subform management

--- main.js
+++ main.js
@@ -1,3 +1,36 @@
 function onChangeMenu(){
     document.getElementById("pop").classList.toggle("pop");
 }
+
+$(document).ready(function(){
+    $('#datepicker').datepicker({
+        format: "dd/mm/yyyy",
+        startDate: '1d',
+        autoclose: true
+    })
+
+    $('#datepicker').on('changeDate', function() {
+        $('#my_hidden_input').val(
+            $('#datepicker').datepicker('getFormattedDate')
+        );
+        $('#date-selected').text(
+            $('#datepicker').datepicker('getFormattedDate')
+        );
+    });
+
+    $('#dates-selected').datepicker({
+        format: "dd/mm/yyyy",
+        startDate: '1d',
+        autoclose: true,
+        multidate: true
+    })
+
+    //Need to configure this for multi dates
+    $('#datepicker').on('changeDate', function() {
+        $('#my_selected_dates').val(
+            $('#datepicker').datepicker('getFormattedDate')
+        );
+    });
+})
+
+
